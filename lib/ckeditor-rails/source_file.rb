75da0de12222c1ba0cd27fa463001565fec66b01
Eric Anderson <eric@pixelwareinc.com>
2012-12-04 16:26:45 -0500

    Support for ckeditor 4.0.
    * All of the js and css fixes are no longer necessary and therefore removed.
      The new ckeditor will compress without errors now.
    * Many files changed causing source_file.rb to be out-of-date. To make it more
      future proof I made an effort to remove dependencies on specific file name.
      This meant major refactoring of source_file.rb. Some of the changes were
      necessary (new download location pattern) while others may be considered more
      aesthetic. Feel free to revert any of my aesthetics you don't find aesthetic.
    * Due to the mass changes required for source_file.rb it is not really possible
      to easily support earlier versions of ckeditor. So no effort was made towards
      that goal.
    * Many of the integration files were removed in ckeditor 4 (asp files,
      php files, etc.). This of course simplied our extraction but it also meant
      the jquery integration was dropped. I don't use jQuery myself but for compat
      with previous versions of this library I have restored it into the lib/assets
      directory. Research online shows that the jQuery integration still works
      with version 4 but I haven't tested it myself.
    * This commit is JUST what is necessary to upgrade to version 4 but doesn't
      include the upgrade itself. The next commit will contain the actual upgrade.

--- lib/ckeditor-rails/source_file.rb
+++ lib/ckeditor-rails/source_file.rb
@@ -5,18 +5,17 @@ class SourceFile < Thor
   source_root 'tmp'
 
   desc "fetch VERSION", "fetch source files from http://ckeditor.com/"
-  def fetch(version)
-    download_url = file_url(version)
-    extract_path = self.class.source_root
-    archive_file = "#{extract_path}/#{filename(version)}"
+  def fetch version
+    download_url = file_url version
+    archive_file = "#{source_root}/#{filename(version)}"
 
     in_root do
-      say_status("       fetch", archive_file, :green)
-      get(download_url, archive_file)
-      if File.exist?(archive_file)
-        FileUtils.mkdir_p(extract_path)
-        extract(archive_file, extract_path)
-        FileUtils.rm_rf(archive_file)
+      say_status "       fetch", archive_file, :green
+      get download_url, archive_file
+      if File.exist? archive_file
+        FileUtils.mkdir_p source_root
+        extract archive_file, source_root
+        FileUtils.rm_rf archive_file
       end
     end
 
@@ -24,117 +23,77 @@ class SourceFile < Thor
       gsub_file "ckeditor-rails/version.rb", /VERSION\s=\s"(\d|\.)+"$/ do |match|
         %Q{VERSION = "#{version}"}
       end
-    end if File.exist?(extract_path)
+    end if File.exist? source_root
   end
 
-  desc "move", "move source files to vendor/assets"
+  desc "move", "move source files"
   def move
-    self.destination_root = "vendor/assets"
+    FileUtils.rm_rf destination_root
 
-    copy_file "ckeditor/adapters/jquery.js", "javascripts/ckeditor/adapters/jquery.js"
-    copy_file "ckeditor/ckeditor.js", "javascripts/ckeditor/ckeditor.js"
-    copy_file "ckeditor/ckeditor_basic.js", "javascripts/ckeditor/ckeditor_basic.js"
-    copy_file "ckeditor/config.js", "javascripts/ckeditor/config.js"
-    copy_file "ckeditor/contents.css", "stylesheets/ckeditor/contents.css"
+    [
+      ['js', 'javascripts'],
+      ['css', 'stylesheets'],
+    ].each do |(type, asset_path)|
+      Dir["#{source_root}/ckeditor/*.#{type}"].each do |file|
+        file = File.basename file
+        copy_file "ckeditor/#{file}", "#{asset_path}/ckeditor/#{file}"
+      end
+    end
 
-    directory "ckeditor/images", "images/ckeditor/images"
+    copy_file "ckeditor/LICENSE.md", "javascripts/ckeditor/LICENSE.md"
     directory "ckeditor/lang", "javascripts/ckeditor/lang"
-
-    copy_file "ckeditor/LICENSE.html", "javascripts/ckeditor/LICENSE.html"
-
-    directory "ckeditor/plugins", "javascripts/ckeditor/plugins"
-    move_image_files_of_plugins
-
-    copy_image_files_of_skin(%w{kama office2003 v2})
-
-    directory "ckeditor/themes", "javascripts/ckeditor/themes"
-  end
-
-  desc "fix_css", "fix some css caused precompilation error"
-  def fix_css
-    self.destination_root = "vendor/assets"
-    inside destination_root do
-      skin_names = %w{kama office2003 v2}
-      skin_names.each do |name|
-        gsub_file "stylesheets/ckeditor/skins/#{name}/dialog.css", /\+margin/, "margin"
-        gsub_file "stylesheets/ckeditor/skins/#{name}/editor.css", /filter\:\;/, ""
-      end
-      gsub_file "stylesheets/ckeditor/skins/office2003/editor.css", /\!height\:28px\;\!line-height\:28px\;/, ""
-      gsub_file "javascripts/ckeditor/plugins/uicolor/yui/assets/yui.css", /\*html/, "* html"
-    end
-  end
-
-  desc 'fix_js', 'fix some js caused execution error'
-  def fix_js
-    self.destination_root = "vendor/assets"
-    inside destination_root do
-      # Change the way IE detected to avoid compilation bug in production
-      %w{ckeditor ckeditor_basic}.each do |name|
-        gsub_file "javascripts/ckeditor/#{name}.js", "/*@cc_on!@*/false", "b.indexOf('msie')>-1"
-      end
-    end
-  end
-
-  desc "convert", "convert css to sass file by sass-convert"
-  def convert
-    self.destination_root = "vendor/assets"
-    inside destination_root do
-      skin_names = %w{kama office2003 v2}
-      skin_names.each do |name|
-        path = "stylesheets/ckeditor/skins/#{name}"
-        run("sass-convert -F css -T sass #{path}/dialog.css #{path}/dialog.css.sass")
-        run("sass-convert -F css -T sass #{path}/editor.css #{path}/editor.css.sass")
-        run("sass-convert -F css -T sass #{path}/templates.css #{path}/templates.css.sass")
-        remove_file "#{path}/dialog.css"
-        remove_file "#{path}/editor.css"
-        remove_file "#{path}/templates.css"
-      end
-    end
+    copy_plugins
+    copy_skins
   end
 
   desc "clean", "clean up useless files"
   def cleanup
-    self.destination_root = "vendor/assets"
-    FileUtils.rm_rf(self.class.source_root)
+    FileUtils.rm_rf source_root
   end
 
   protected
 
-  def file_url(version)
+  def file_url version
     "http://download.cksource.com/CKEditor/CKEditor/CKEditor%20#{version}/#{filename(version)}"
   end
 
-  def filename(version)
-    "ckeditor_#{version}.tar.gz"
+  def filename version
+    "ckeditor_#{version}_full.tar.gz"
   end
 
-  def extract(file_path, output_path)
-    system("tar --exclude=*.php --exclude=*.asp -C '#{output_path}' -xzf '#{file_path}' ckeditor")
+  def extract file_path, output_path
+    system "tar -C '#{output_path}' -xzf '#{file_path}' ckeditor"
   end
 
-  def move_image_files_of_plugins
-    files = Dir.glob("#{destination_root}/javascripts/ckeditor/plugins/*/*/*")
+  def copy_plugins
+    Dir["#{source_root}/ckeditor/plugins/*"].each do |plugin|
+      copy_all "plugins/#{File.basename plugin}"
+    end
+  end
+
+  def copy_skins
+    Dir["#{source_root}/ckeditor/skins/*"].each do |skin|
+      copy_all "skins/#{File.basename skin}"
+    end
+  end
+
+  def copy_all path
+    copy_type path, 'css', 'stylesheets'
+    copy_type path, 'js', 'javascripts'
+    copy_type path, 'png', 'images'
+    copy_type path, 'gif', 'images'
+    copy_type path, 'jpg', 'images'
+  end
+
+  def copy_type path, type, asset_path
+    files = Dir["#{source_root}/ckeditor/#{path}/**/*.#{type}"]
     files.each do |file|
-      if file =~ /\.(png|gif)$/
-        filename = file.sub("#{destination_root}/javascripts/", '')
-        destination_path = filename.sub(/\/\w+\.\w+$/, '')
-        in_root do
-          FileUtils.mkdir_p("#{destination_root}/images/#{destination_path}")
-          FileUtils.mv(file, "#{destination_root}/images/#{filename}")
-        end
-      end
+      file = file.sub /^#{Regexp.escape source_root}\//, ''
+      copy_file file, "#{asset_path}/#{file}"
     end
   end
 
-  def copy_image_files_of_skin(skin_names = %w{kama office2003 v2})
-    skin_names.each do |name|
-      copy_file "ckeditor/skins/#{name}/dialog.css", "stylesheets/ckeditor/skins/#{name}/dialog.css"
-      copy_file "ckeditor/skins/#{name}/editor.css", "stylesheets/ckeditor/skins/#{name}/editor.css"
-      copy_file "ckeditor/skins/#{name}/icons.png", "images/ckeditor/skins/#{name}/icons.png"
-      copy_file "ckeditor/skins/#{name}/icons_rtl.png", "images/ckeditor/skins/#{name}/icons_rtl.png"
-      directory "ckeditor/skins/#{name}/images", "images/ckeditor/skins/#{name}/images"
-      copy_file "ckeditor/skins/#{name}/skin.js", "javascripts/ckeditor/skins/#{name}/skin.js"
-      copy_file "ckeditor/skins/#{name}/templates.css", "stylesheets/ckeditor/skins/#{name}/templates.css"
-    end
+  def source_root
+    self.class.source_root
   end
 end
