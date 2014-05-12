b03b92883dd6df6667f8c141d2f7ad42b867e0f6
Yashchuk Oleg <oazoer@gmail.com>
2014-05-12 17:52:35 +0400

    Move engine config into initializer

--- lib/ckeditor-rails/engine.rb
+++ lib/ckeditor-rails/engine.rb
@@ -1,14 +1,16 @@
 module Ckeditor
   module Rails
     class Engine < ::Rails::Engine
-      config.assets.precompile += %W(
-        ckeditor/*.js
-        ckeditor/*.css
-        ckeditor/*.png
-        ckeditor/*.gif
-        ckeditor/*.html
-        ckeditor/*.md
-      )
+      initializer "ckeditor_rails.assets_precompile", :group => :all do |app|
+        app.config.assets.precompile += %W(
+          ckeditor/*.js
+          ckeditor/*.css
+          ckeditor/*.png
+          ckeditor/*.gif
+          ckeditor/*.html
+          ckeditor/*.md
+        )
+      end
 
       rake_tasks do
         load "ckeditor-rails/tasks.rake"
